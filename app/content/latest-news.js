4cc610c127943380b32ee9e7005fdebb714162e2
uavictory <uavictorycountdown@gmail.com>
2022-05-23 11:13:28 -0400

    news

--- app/content/latest-news.js
+++ app/content/latest-news.js
@@ -2,12 +2,20 @@
  */
 const latestNews = [
   {
-    t: `Polish President Duda returns to Kyiv`,
-    url: 'https://www.rferl.org/a/ukraine-poland-duda-visit/31862055.html',
+    t: `Poland terrminates Russian gas contract`,
+    url: 'https://www.euronews.com/2022/05/23/ukraine-crisis-poland-russia',
   },
   {
-    t: `Russians try to cross Siverskyi Donets river again! Reports they lost whole BTG - again! `,
-    url: 'https://twitter.com/EuromaidanPress/status/1528330661752348677',
+    t: 'May 23 Russian loss estimates: +150 troops, +8 tanks, +25 APV, +6 drones...',
+    url: 'https://twitter.com/KyivIndependent/status/1528644115008733185',
+  },
+  {
+    t: `May 22 ISW Report: Russians minimal gains in Kharkiv,East. Prepaing more bother in South`,
+    url: 'https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-may-22',
+  },
+  {
+    t: `Polish President Duda returns to Kyiv`,
+    url: 'https://www.rferl.org/a/ukraine-poland-duda-visit/31862055.html',
   },
   {
     t: `Russian concert-goers in St. Petersburg: Fuck War`,
@@ -18,17 +26,119 @@ const latestNews = [
     url: 'https://thehill.com/news/administration/3495301-biden-signs-40b-ukraine-aid-package-into-law/',
   },
   {
-    t: 'May 22 Russian loss estimates: +200 troops, +7 tanks, +25 APV, +8 drones...',
-    url: 'https://twitter.com/KyivIndependent/status/1528301735529353216',
-  },
-  {
-    t: `May 21 ISW Report: Russians continue to try to surround Seveodonetsk. Stalled elsewhere`,
-    url: 'https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-may-21',
-  },
-  {
     t: `UNSTOPPABLE: Kharkiv cat Stepan follows up Ukraine Eurovision triumph with Influencer Award win`,
     url: 'https://twitter.com/KyivIndependent/status/1527236169251049473',
   },
+];
+
+const pastNews = [
+  {
+    date: '2022-05-25',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-24',
+    items: [
+      {
+        t: `Russians try to cross Siverskyi Donets river again! Reports they lost whole BTG - again! `,
+        url: 'https://twitter.com/EuromaidanPress/status/1528330661752348677',
+      },
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-23',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-22',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-21',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-20',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-19',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-18',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-17',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-16',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+  {
+    date: '2022-05-15',
+    items: [
+      {
+        t: ``,
+        url: ``,
+      }
+    ],
+  },
+]
+export {latestNews, pastNews};
+
+/* past
   {
     t: `Liberation of Kharkiv Continues`,
     url: 'https://twitter.com/KyivIndependent/status/1526988884026040320',
@@ -45,22 +155,6 @@ const latestNews = [
     t: "Poland issues stamp commemorating Zelensky's leadership",
     url: 'https://twitter.com/ukraine_world/status/1524718178261258246',
   },
-];
-
-const pastNews = [
-  {
-    date: '2022-05-22',
-    items: [
-      {
-        t: ``,
-        url: ``,
-      }
-    ],
-  },
-]
-export {latestNews, pastNews};
-
-/* past
   {
     t: `Who won eurovision 2022? hint: they also won the battles of kyiv and kharkiv`,
     url: 'https://www.bbc.com/news/entertainment-arts-61452678',
