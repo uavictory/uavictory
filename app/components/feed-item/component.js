e9989e2136ab07b71e4e81418b04c204276857cb
uavictory <uavictorycountdown@gmail.com>
2022-05-25 18:27:53 -0400

    cleanup

--- app/components/feed-item/component.js
+++ app/components/feed-item/component.js
@@ -3,7 +3,6 @@ import consts from '../../consts';
 
 export default class FeedItemComponent extends Component {
 
-
     get imgSrc() {
         const src = `${consts.imageBaseUrl}/visitors/${this.args.item.i}`;
         return src; 
