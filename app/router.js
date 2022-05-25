5f6ad6882d6e1462125e2f2ec0271ba052c9893d
uavictory <uavictorycountdown@gmail.com>
2022-05-25 18:27:48 -0400

    add spirit route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('index', { path: '/' });
+  this.route('feed', {path: 'spirit'} );
   this.route('heroes');
   this.route('armaments');
   this.route('battlefields');
