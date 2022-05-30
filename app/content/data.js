63eae033a30019e74dc9dfbbb1d87a7f7703e692
uavictory <uavictorycountdown@gmail.com>
2022-05-29 22:03:59 -0400

    other data to add

--- app/content/data.js
+++ app/content/data.js
@@ -1,3 +1,16 @@
+/*
+sanctions
+*/
+/*
+pre-war strength
+https://www.statista.com/topics/9087/russia-ukraine-war-2022/#dossierKeyfigures
+*/
+
+/*
+russian losses
+https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html
+- can scrape
+*/
 const items = [
     {
         t: 'Weapons supplies to Ukraine',
