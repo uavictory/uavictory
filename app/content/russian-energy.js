3b11fbd3e2d657d2d1d1e8038c92501bd8330719
uavictory <uavictorycountdown@gmail.com>
2022-05-30 10:20:54 -0400

    energy

---
+++ app/content/russian-energy.js
@@ -0,0 +1,11 @@
+/*
+ denmake to end oil https://twitter.com/nexta_tv/status/1531250842740961280
+ https://twitter.com/jay_the_cee/status/1531254168907919360
+*/
+
+/*
+ russian casualties
+
+ indigenous overrep
+ https://twitter.com/kamilkazani/status/1531238266283900932
+*/
