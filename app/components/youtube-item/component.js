54a03580b81e686c0ad901acdf93f01a8cdae6cf
uavictory <uavictorycountdown@gmail.com>
2022-06-03 13:18:57 -0400

    fix song display on mobile

--- app/components/youtube-item/component.js
+++ app/components/youtube-item/component.js
@@ -4,4 +4,7 @@ export default class YoutubeItemComponent extends Component {
   item = this.args.item;
   vid = this.item.key;
   src = `https://www.youtube.com/embed/${this.vid}`;
+  ytWidth = 380;
+  ytWidthPx = `${this.ytWidth}px`
+  ytHeightPx = `${this.ytWidth*9/16}px`
 }
