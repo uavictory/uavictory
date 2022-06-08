403b70e7b9fb2c5697fd72c9641fafd24211c07b
uavictory <uavictorycountdown@gmail.com>
2022-06-08 18:14:38 -0400

    why ukraine will win placement

---
+++ app/content/why-ukraine-wins.js
@@ -0,0 +1,36 @@
+const why = `
+  <p>
+    Does anyone still believe Ukraine can't win? Maybe Henry Kissinger. The Russians don't
+    believe it. Just listen to the intercepts from their soldier. Or the increasingly 
+    hysterical claims made on Russian prime time media about having to de-militarize all of 
+    Nato. The Russians know reality. It's too much to bear. So they retreat into fantasy land.
+  </p>
+  <p>
+    The Russians are fighting for the delusion of a renewed Russian Empire -- a delusion many
+    Russians would welcome were it free, but for which few are willing to pay. Give the soldiers 
+    a choice, and most would go home.
+  </p>
+  <p>
+    The Ukrainians are fighting for their land, their homes, their families.
+    Their whole society is represented in the field: teachers, musicians, boxers, professors, 
+    MPs, even a Supreme Court Justice. The leadership of Zelenskyy, Kuleba, Zaluzhnyi makes
+    Putin, Shoigu and cronies look like a clown parade. Ukrainian soldiers are tougher,
+    smarter and know how to take initiative. They are running circles around their low-morale
+    centrally-micromanaged Russian counterparts.
+  </p>
+  <p>
+    None of this matters when one side has a huge advantage in guns. Without support,
+    Ukrainian forces would be ground down by the huge, shambling Russian army, and
+    stalemate would be the best hope.
+  </p>
+  <p>
+    But the military equation has changed. Russia is suffering unsustainable losses,
+    and much of their lost equipment is being captured and reused by 
+    Ukraine. Add to this the huge quantities of heavy Western weapons -- including 
+    longer-range artillery and MLRS -- and the prospects for the Russian army 
+    are grim indeed. The ZSU estimates the tide will turn in August, and the hot 
+    phase of the war will be done by year end. We think things will happen sooner. 
+    But either way don't doubt: Україна переможе.
+  </p>
+`
+export default why;
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,19 @@
 import Controller from '@ember/controller';
 import { latestNews } from '../content/latest-news';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+import why from '../content/why-ukraine-wins';
+
+const hidden = 'hidden';
 
 export default class IndexController extends Controller {
   latest = latestNews;
+  whyBlurb = why; 
+  @tracked hideBlurbClass = hidden;
+
+  @action
+  toggleWhyWin() {
+    this.hideBlurbClass = (!this.hideBlurbClass ? hidden : '');
+  }
 }
