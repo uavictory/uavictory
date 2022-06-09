618f3c27614c65de5f41708ce13c1c8457129aa3
uavictory <uavictorycountdown@gmail.com>
2022-06-09 19:58:30 -0400

    sample content

---
+++ app/content/most.js
@@ -0,0 +1,52 @@
+/*
+  Most Badass Old Layd
+  schwarzhenhagger
+  Zelenskii
+  Klitschos
+  15 yo boy
+  Best Reply
+  Best Stamp
+  Best Tractor
+  Best Bayraktar Rendition
+  Biggest Clowns
+  Patron
+  Kharkiv Cat
+  Baddest Ambush Footage
+*/
+const items = [
+  {
+    t: 'Most Badass Old Lady in Ukraine',
+    type: 'yt',
+    key: 'L17Bi7zBJHI',
+    txt: `
+        <p>
+          Who are you? Are you Russians? You're occupiers, fascists. What the
+          fuck are you doing on our land with all these guns?
+        </p>
+        <p>
+          Take these seeds and put them in your pockets so at least sunflowers
+          will grow when you all lie down here.
+          Put the sunflower seeds in your pocket please. You will lie down
+          here with the seeds.
+
+          From this moment you're cursed. Pieces of shit.
+
+        </p>
+        `,
+  },
+  {
+    t: 'Most Heavyweight Champions in the War',
+    type: 'img',
+    key: 'people/klit-m_3nVN6MTVaP.png',
+    txt: `
+        <p>
+          Between them Vitaly and Gennady Klitshco have xx world titles.
+        </p>
+        <p>
+          Vitaly is mayor. Gennady.
+        </p>
+        `,
+  },
+];
+
+export default items;
