174cf4d87052a488628c294518cfef2259407ace
uavictory <uavictorycountdown@gmail.com>
2022-06-10 17:36:17 -0400

    Most!!
    todo: cleanup the CSS and the cards!!!!

--- app/content/most.js
+++ app/content/most.js
@@ -1,18 +1,11 @@
 /*
-  Most Badass Old Layd
-  schwarzhenhagger
-  Zelenskii
-  Klitschos
-  15 yo boy
-  Best Reply
-  Best Stamp
-  Best Tractor
-  Best Bayraktar Rendition
-  Biggest Clowns
-  Patron
-  Kharkiv Cat
   Baddest Ambush Footage
+  15 yo boy
+  Best Stamp
+  Best Bayraktar Rendition
+  most no-nonsense military spokesmen
 */
+
 const items = [
   {
     t: 'Most Badass Old Lady in Ukraine',
@@ -35,15 +28,170 @@ const items = [
         `,
   },
   {
-    t: 'Most Heavyweight Champions in the War',
-    type: 'img',
-    key: 'people/klit-m_3nVN6MTVaP.png',
+    t: 'Best Tractor',
+    type: 'yt',
+    key: 'AmPD1w1bZVI',
     txt: `
         <p>
-          Between them Vitaly and Gennady Klitshco have xx world titles.
+          The original, or at least the first one we remember seeing, the one
+          that set off the tractor craze, is still the best.
         </p>
         <p>
-          Vitaly is mayor. Gennady.
+          These were they carefree days before Bucha, when the Russians soldiers
+          seemed more like a bunch of lost circus performers, rather than the
+          murderous thugs and looters they are.
+        </p>
+        `,
+  },
+  {
+    t: 'Best Retort',
+    type: 'yt',
+    key: '_1J65DWUIns',
+    txt: `
+        <p>
+          "Russian warship to Snake Island...I propose you lay down
+          your arms and surrender..."
+        </p>
+        <p>
+          Response: Russian warship, idi na khuy
+        </p>
+        <p>
+          Can anyone explain what that means in Ukrainian? :-)
+        </p>
+        `,
+  },
+  {
+    t: 'Greatest Wartime Leader since WWII',
+    type: 'img',
+    key: 'most/zelensky-stays-in-kyiv_B634ftx1Q.jpeg',
+    txt: `
+        <p>
+          When the US offered to evacuate Ukrainian President Zelensky, his
+          responded "I need ammunition, not a ride", the most defiant rallying 
+          call since Churchill's "We'll fight them on the beaches" speech.
+        </p>
+        <p>
+          He continues to be Ukraine's focal point, fearlessly walking Kyiv's streets, 
+          rallying the world to Ukraine's defense, visiting the front lines, and 
+          capturing the historical nature of Ukrainian's heroic struggle.
+        </p>
+        <p>
+          No one expected this from (admittedly a very well-educated capable) former
+          comedian, least of all Putin who, despite tyrannizing Russia and
+          the world for over 20 years, is clearly overmatched.
+        </p>
+        `,
+  },
+  {
+    t: 'Most Moving Appeal to Ordinary Russians to Choose Peace',
+    type: 'yt',
+    key: 'fWClXZd9c78',
+    txt: `
+        <p>
+          Arnold Schwarzenegger has a long positive history with Russia. 
+        </p>
+        <p>
+          Here he gives a moving appeal to ordinary Russians to oppose Putin's war.
+        </p>
+        `,
+  },
+  {
+    t: 'Most Heavyweight Champions ot the War',
+    type: 'yt',
+    key: 'iV_Jg6E_gQM',
+    txt: `
+        <p>
+          Vitaly and Wladimir Klitschko dominated heavyweight boxing
+          for nearly a decade.
+        </p>
+        <p>
+          Now they're defending their country, Vitaly as mayor of Kyiv
+          and Wladimir as member of the Ukrainian Armed Forces.
+        </p>
+        `,
+  },
+  {
+    t: 'Best Victory Day Speech',
+    type: 'yt',
+    key: 'Ky796BUuB0s',
+    txt: `
+        <p>
+          Yay its him again. Winning the war of hearts and minds.
+        </p>
+        <p>
+          Who really bore the brunt of the Nazi invasion of the USSR? Ukraine.
+          And you will not take that history and sacrife away from us.
+        </p>
+        `,
+  },
+  {
+    t: 'Mine-sniffing Goat',
+    type: 'yt',
+    key: 'J5mi0zskgNo',
+    txt: `
+        <p>
+          That's GOAT as in Greatest of All Times, for Patron is a Jack Russell
+          terrier who has saved countless lives by helping Ukrainian forces 
+          sniff out mines left by the Russians
+        </p>
+        <p>
+          For his efforts, both Patron and his owner have been awarded the Order
+          for Courage by President Zelensky.
+        </p>
+        `,
+  },
+  {
+    t: 'Most Influential Cat',
+    type: 'img',
+    key: 'feed/stepan_wins_lcxxO5wvj.jpeg',
+    txt: `
+        <p>
+          Kharkiv cat Stepan has over a million social media followers. He has
+          helped raise tens of thousands of dollars for Ukraine and recently won
+          the purrrrrestigious World Influencers and Bloggers Award in Cannes, 
+          France.  
+        </p>
+        <p>
+          Not bad for a cat.
+        </p>
+        `,
+  },
+  {
+    t: 'Clearest Signs',
+    type: 'img',
+    key: 'most/go-to-street-signs_t7-x142DFn.jpeg',
+    txt: `
+        <p>
+          To help the Russian invasion forces reach their objectives, the
+          Ukrainians put up some signs with very clear direction:
+        </p>
+
+        <p>
+            &nbsp;&nbsp;Go fuck yourself<br/>
+            &nbsp;&nbsp;Go fuck yourself again<br/>
+            &nbsp;&nbsp;Go fuck yourself back to Russia<br/>
+        </p>
+        <p>
+          Yah, yah we've heard the quibbles this was photoshopped, but surely 
+          Russian troops use the internet and are getting a clear message.
+        </p>
+        `,
+  },
+  {
+    t: 'Biggest Clowns',
+    type: 'yt',
+    key: '9Xoo_Hfx5jo',
+    txt: `
+        <p>
+          Reportedly some 'elite' members of Chechen clown-master Ramzan Kadyrov's forces were
+          sent to assassinate Zelensky before they themselves were eliminated. Since then, Russia's
+          Chechen units have taken to Tik-Tok, making videos of themselves 'fighting' against enemies
+          like...traffic lights and empty building. Clown parade.
+        </p>
+        <p>
+          We should also mention that, unlike Kadyrov's clowns, many brave Chechens are renewing 
+          their independence struggle against Russia, and fighting and dying both for the freedom 
+          of Ukraine and their own.
         </p>
         `,
   },
