d215295afcad56186367e1b9c703476ccdfa7149
uavictory <uavictorycountdown@gmail.com>
2022-06-24 09:34:59 -0400

    officer losses

--- app/content/data/russian-officer-losses.js
+++ app/content/data/russian-officer-losses.js
@@ -1,5 +1,5 @@
 export const dataLink = {
-  url: 'https://pbs.twimg.com/media/FVMwSyyXsAA6Q6X?format=png',
-  updated: '2022-06-07'
+  url: 'https://pbs.twimg.com/media/FV_vca1WIAEJc8E?format=png',
+  updated: '2022-06-24'
 }
 // https://twitter.com/KilledInUkraine
