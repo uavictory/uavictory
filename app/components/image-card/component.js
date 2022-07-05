958265aa5dc329bbfe8bf6af69dc0efc93b2bcb7
uavictory <uavictorycountdown@gmail.com>
2022-07-05 13:00:51 -0400

    image links for image-cards

--- app/components/image-card/component.js
+++ app/components/image-card/component.js
@@ -2,8 +2,10 @@ import Component from '@glimmer/component';
 import consts from '../../consts';
 
 export default class ImageCardComponent extends Component {
+    item = this.args.item;
+
     get imgSrc() {
-        const src = `${consts.imageBaseUrl}/${this.args.item.key}`;
+        const src = `${consts.imageBaseUrl}/${this.item.key}`;
         return src; 
     }
 }
